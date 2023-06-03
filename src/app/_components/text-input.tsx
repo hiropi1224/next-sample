10d01f3cac8379223d48565e710ee77759336fb9
Shirahamah <hiropi1224s@gmail.com>
2023-06-03 17:58:40 +0900

    💄 inputの遅延処理を試行錯誤

---
+++ src/app/_components/text-input.tsx
@@ -0,0 +1,41 @@
+'use client';
+import React, { FC, useEffect, useState, useTransition } from 'react';
+import { sleep } from '@/app/_utils/sleep';
+
+export const TextInput: FC = () => {
+  const [text, setText] = useState('');
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (text !== '') {
+        // API呼び出し処理
+        alert(`call API:${text}`);
+      }
+    }, 1000);
+
+    // cleanup
+    return () => clearTimeout(timer);
+  }, [text]);
+
+  return (
+    <div>
+      <div className='flex w-1/3'>
+        <input
+          className='w-full flex-1 inline-flex items-center justify-center rounded py-2 text-base shadow-sm focus:shadow'
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+            startTransition(async () => {
+              await sleep(1000);
+            });
+          }}
+        />
+        <div>{isPending ? <>...loading</> : <></>}</div>
+      </div>
+      <div>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
---
+++ src/app/form/page.tsx
@@ -0,0 +1,12 @@
+import { TextInput } from '@/app/_components/text-input';
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <p>Hello, world!</p>
+        <TextInput />
+      </div>
+    </main>
+  );
+}
