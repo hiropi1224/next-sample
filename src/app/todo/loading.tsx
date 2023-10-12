4d4e3d6a1a0ea6bc8b2659b7b5d6d0c11e91df89
Shirahamah <hiropi1224s@gmail.com>
2023-10-13 00:03:44 +0900

    loadingファイル削除

--- src/app/todo/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-  return (
-    <div className='min-h-screen flex justify-center items-center'>
-      <div className='h-2 w-2 animate-ping rounded-full bg-pink-600'></div>
-      <div className='mx-4 h-2 w-2 animate-ping rounded-full bg-pink-600'></div>
-      <div className='h-2 w-2 animate-ping rounded-full bg-pink-600'></div>
-    </div>
-  );
-}
